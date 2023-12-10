4c082667cb628a529dc0f8b7bab7db46f16f688d
Telekinesis846 <kieranbeard19@gmail.com>
2023-12-10 14:22:47 +0000

    apparently the querySelector returns null!

--- content.js
+++ content.js
@@ -1,12 +1,22 @@
+function onSkipClick() {
+    alert("Jee");
+}
+
 function addButton() {
     const skip = document.createElement("button");
     skip.textContent = "Skip";
     skip.style = `
-        position: absolute;
-        top: -2px
+        /* position: absolute;
+        top: -2px; */
+    
+        background-color: white;
+        border: 10px white;
     `;
+    skip.onClick = onSkipClick;
 
-    document.querySelector("body").appendChild(skip);
+    document
+        .querySelector("._PackageTypeDescription_1ehfm_32")
+        .appendChild(skip);
 }
 
 const href = location.href;
