d0a7ea6607440fb861a66caa8b2a316ca6064adc
Telekinesis846 <kieranbeard19@gmail.com>
2023-12-10 14:12:13 +0000

    made the button appear on sparx pages

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-console.log("Yay");
+function addButton() {
+    const skip = document.createElement("button");
+    skip.textContent = "Skip";
+    skip.style = `
+        position: absolute;
+        top: -2px
+    `;
+
+    document.querySelector("body").appendChild(skip);
+}
+
+const href = location.href;
+
+if (href.includes("sparxmaths")) {
+    addButton();
+}
